27bea7eb3cd9491fab9625629c5183dcea62ad7d
Sinh Luu <35860244+luuvansinh@users.noreply.github.com>
2022-12-04 15:57:13 +0700

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World! V3")
+		return c.String(http.StatusOK, "Hello, World! V4")
 	})
 	e.Logger.Fatal(e.Start(":3000"))
 }
